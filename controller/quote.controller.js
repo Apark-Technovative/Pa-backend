c0194adf876443448f9312473b4681e1efc8857c
rockey223 <rockeym50@gmail.com>
2026-01-26 23:14:38 +0545

    added different layout for quote

--- controller/quote.controller.js
+++ controller/quote.controller.js
@@ -5,19 +5,30 @@ const path = require("path");
 
 exports.sendQuote = async (req, res) => {
   try {
-    const { title, type1, type2, price,email } = req.body;
-    const filePath = path.join(__dirname, "../mailContent/quote.html");
-
-    var html = await fs.readFile(filePath, "utf-8");
-
-    html = html.replace("[title]", title);
-    html = html.replace("[type1]", type1);
-    html = html.replace("[type2]", type2);
-    html = html.replace("[price]", price);
+    const { title, type1, type2, price, email } = req.body;
+    const quote = path.join(__dirname, "../mailContent/quote.html");
+    const bankingQuote = path.join(
+      __dirname,
+      "../mailContent/bankingQuote.html",
+    );
+    var html;
+    if (title == "Banking Mortgage Loans in Nepal") {
+      console.log(price);
+      
+      html = await fs.readFile(bankingQuote, "utf-8");
+      html = html.replace("[title]", title);
+      html = html.replace("[price]", price);
+    } else {
+      html = await fs.readFile(quote, "utf-8");
+      html = html.replace("[title]", title);
+      html = html.replace("[type1]", type1);
+      html = html.replace("[type2]", type2);
+      html = html.replace("[price]", price);
+    }
 
     const info = await emailTransporter.sendMail({
       from: process.env.EMAIL_SENDER,
-      to: process.env.EMAIL_SENDER,
+      to: email,
       subject: "web Mail",
       html: html,
     });
@@ -27,7 +38,7 @@ exports.sendQuote = async (req, res) => {
         type1: type1 ? type1 : "",
         type2: type2 ? type2 : "",
         price: price,
-        email
+        email,
       });
       res.status(200).json({ message: "Quote sent successfully" });
     } else {
